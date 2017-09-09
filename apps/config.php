bd87e3b55b0492e76ae7fcaa248ae48780d1a9cb
ZhaoJunfeng <981248356@qq.com>
2017-09-09 12:46:58 +0800

    更新配置路由

--- apps/config.php
+++ apps/config.php
@@ -108,7 +108,7 @@ $config = [
     // 路由使用完整匹配
     'route_complete_match'   => false,
     // 路由配置文件（支持配置多个）
-    'route_config_file'      => ['route','route_sso'],
+    'route_config_file'      => ['route'],
     // 是否强制使用路由
     'url_route_must'         => false,
     // 域名部署
