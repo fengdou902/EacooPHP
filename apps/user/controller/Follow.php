a5cfb4f1475552aeae0531498aba185e50abaf00
Hackxiaoya <hackxiaoya@gmail.com>
2018-01-07 00:03:38 +0800

    修正User通用钩子中间层；修正关注插件残留；修正用户列表URL错误；

--- apps/user/controller/Follow.php
+++ apps/user/controller/Follow.php
@@ -24,26 +24,34 @@ class Follow extends Home{
      * @return [type] [description]
      * @date   2018-01-06
      * @author 心云间、凝听 <981248356@qq.com>
+     * {:URL('user/follow/index',['hook'=>'attention','type'=>'watchlist','uid'=>$info.uid])}
+     * 参数：hook是插件名字 *必填
+     * 参数：type是插件方法 *必填
+     * 参数：uid选填项，可不填
      */
-    public function test()
-    {
+    public function index(){
+
+      hook(input('hook'),input(),true);
+
         //例子1：获取关注数量
-        $result = hook('attention',[],true);
-        dump($result);
+        /*$result = hook('attention',[],true);
+        dump($result);*/
 
         //例子2：获取Ta关注数量
-        $result = hook('attention',['uid'=>2],true);
-        dump($result);
+        //$result = hook('attention',['type'=>'watchlist'],true);
+        //dump($result);
+
+      //hook('attention',['type'=>'fanslist'],true);
 
         // 非钩子方法，在需要的地方调用该方法：
-        $plugin_class = get_plugin_class('attention');
+        /*$plugin_class = get_plugin_class('attention');
         if (!class_exists($plugin_class)) {
             echo 'fail';
             exit;
         }
         $plugin_obj    = new $plugin_class;
         $result = $plugin_obj->getData();
-        dump($result);
+        dump($result);*/
     }
 
 }
