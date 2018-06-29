cc8acb4a678166c8548a56ef1930ac5c28e19764
ZhaoJunfeng <981248356@qq.com>
2018-06-30 00:19:30 +0800

    修改域名信息、重置主题样式

--- README.md
+++ README.md
@@ -80,7 +80,7 @@ return builder('List')
 ### 前端组件
 artTemplate(JS模版引擎),artDialog(弹窗),datetimepicker(日期),echarts(图表),colorpicker(颜色选择器),fastclick,iCheck(复选框美化),ieonly,imgcutter,jquery-repeater,lazyload(延迟加载),select2,superslide,ueditor,wangeditor,webuploader,x-editable
 
-官网：[http://www.eacoo123.com](http://www.eacoo123.com)
+官网：[http://www.eacoophp.com](http://www.eacoophp.com)
 QQ群: 436491685
 ### 演示地址
 [http://demo1.eacoo123.com/admin](http://demo1.eacoo123.com/admin)  
@@ -98,5 +98,5 @@ GitHub：[https://github.com/fengdou902/EacooPHP.git](https://github.com/fengdou
 ### 版权申明
 EacooPHP遵循Apache2开源协议发布，并提供免费使用。  
 本项目包含的第三方源码和二进制文件之版权信息另行标注。  
-版权所有Copyright © 2017-2018 by EacooPHP (http://www.eacoo123.com)  
+版权所有Copyright © 2017-2018 by EacooPHP (http://www.eacoophp.com)  
 All rights reserved。
--- apps/admin/logic/Module.php
+++ apps/admin/logic/Module.php
@@ -1,7 +1,7 @@
 <?php
 // 后台模块处理逻辑      
 // +----------------------------------------------------------------------
-// | Copyright (c) 2017-2018 http://www.eacoo123.com, All rights reserved.         
+// | Copyright (c) 2017-2018 http://www.eacoophp.com, All rights reserved.         
 // +----------------------------------------------------------------------
 // | [EacooPHP] 并不是自由软件,可免费使用,未经许可不能去掉EacooPHP相关版权。
 // | 禁止在EacooPHP整体或任何部分基础上发展任何派生、修改或第三方版本用于重新分发
@@ -91,7 +91,7 @@ class Module extends AdminLogic
             switch($val['status']){
                 case -3:  // 模块信息异常
                     $val['status'] = '<span class="text-danger">异常</span>';
-                    $val['right_button']  = '<a class="btn btn-danger btn-sm" href="http://forum.eacoo123.com" target="_blank">反馈</a>';
+                    $val['right_button']  = '<a class="btn btn-danger btn-sm" href="http://forum.eacoophp.com" target="_blank">反馈</a>';
                     break;
                 case -2:  // 损坏
                     $val['status'] = '<span class="text-danger">损坏</span>';
